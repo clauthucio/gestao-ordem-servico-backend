0ba9f02583ca5459606518c32b7bf27b78647e83
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 22:33:52 -0400

    Remove o Try Catch de Dashboard Service

--- src/services/dashboardService.ts
+++ src/services/dashboardService.ts
@@ -1,29 +1,19 @@
 import { DataSource, Repository, Between, MoreThanOrEqual } from "typeorm";
 import { appDataSource } from "../database/appDataSource";
-import { OrdemServico } from "../entities/OrdemServico";
+import { OrdemServico } from "../entities/ordemServicoEntity";
 import { enumPrioridade } from '../types/Prioridade';   
 import { enumStatus } from '../types/Status';
-import { AppError } from "../errors/AppError"; //ESTUDAR
 
 export class DashboardService {
     private ordemRepository: Repository <OrdemServico>
     constructor(){
         this.ordemRepository = appDataSource.getRepository(OrdemServico);
     }
-    async getIndicadores(){
-        try{
-            const totalAbertasHoje = await this.obterTotalAbertasHoje();
+    public async getIndicadores(){
+        const totalAbertasHoje = await this.obterTotalAbertasHoje();
             return {
-                indicadores: {
-                    totalAbertasHoje
-                 },
-                 ordemRecentes:[]
-
-                };
-
-                }   catch (error){
-                throw new Error(`Erro ao buscar indicadores: ${error}` )
-            } 
+                indicadores: {totalAbertasHoje},ordemRecentes:[]
+            };
         }
     private async obterTotalAbertasHoje(): Promise<number> {
         const hoje = new Date();
