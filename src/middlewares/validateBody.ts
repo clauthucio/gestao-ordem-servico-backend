45e80f82401091c0ca5698e80984cfdeff62351c
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 20:24:50 -0400

    Adicionando middleware "validateBody.ts" para tratar o body dos payloads de CRUD

---
+++ src/middlewares/validateBody.ts
@@ -0,0 +1,15 @@
+import type { RequestHandler } from "express";
+import type { ZodSchema } from "zod";
+import { AppError } from "../errors/AppError";
+
+export const validateBody = (schema: ZodSchema): RequestHandler => {
+    return (req, res, next) => {
+        const result = schema.safeParse(req.body);
+        if (!result.success){
+            return next(new AppError ("Dados Inválidos", 400, result.error.flatten()));
+        }
+
+        req.body = result.data;
+        return next();
+    }
+}
