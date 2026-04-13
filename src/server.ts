14c45b1705f137b136a0c7862d9abfc7081558d1
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 22:30:42 -0400

    Atualiza o Server com a Rota de Ordem de Servico

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import 'reflect-metadata';
-import express from 'express';
 import 'dotenv/config';
+import 'reflect-metadata';
 import { appDataSource } from './database/appDataSource.js';
 import { dashboardRoute } from './routes/dashboardRoute.js';
-import { usuarioRoute } from './routes/usuarioRoute.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { ordemServicoRoute } from './routes/ordemServicoRoute.js';
+import { usuarioRoute } from './routes/usuarioRoute.js';
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(dashboardRoute);
 app.use(usuarioRoute);
+app.use(ordemServicoRoute);
 app.use(errorHandler);
 
 
