ec5b9f81cb11f9d363b983e0f1290b7baf327d9f
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 22:32:15 -0400

    Remove o try catch do Dashboar Controller -

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -1,26 +1,13 @@
-import {Request, Response} from "express";
 import {DashboardService} from "../services/dashboardService";
+import {Request, Response} from "express";
 
 export class DashboardController {
 
     constructor (private dashboardService: DashboardService) {}
     
-    async getDashboard (req: Request, res: Response){
-        try {
-            //Chama o service para buscar os dados
-            const dados = await this.dashboardService.getIndicadores();
-
-            //Retorna sucesso
-            res.status(200).json(dados);
-
-        } catch (error){
-            //Tratamento de erro
-            console.error(error);
-            res.status(500).json({
-                erro: "Erro ao buscar dasboard",
-                mensagem: error instanceof Error ? error.message: "Erro desconhecido"
-            }
-            )
-        }
-    }
+    public async getDashboard (req: Request, res: Response) {
+        const dados = await this.dashboardService.getIndicadores();
+        res.status(200).json(dados);
+    };
 }
+    
