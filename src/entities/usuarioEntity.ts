b39b0b7f4d4116200f55a592e898271d6d31a86d
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 20:26:40 -0400

    Renomea Entidade Usuario

---
+++ src/entities/usuarioEntity.ts
@@ -0,0 +1,31 @@
+import {Column, Entity, PrimaryGeneratedColumn, JoinColumn, ManyToOne} from "typeorm"
+import {enumPerfil} from "../types/Perfil"
+import { Equipamento } from "./equipamentoEntity";
+
+@Entity("usuario")
+export class Usuarios {
+    @PrimaryGeneratedColumn("uuid")
+    idUsuario!: string;
+    
+    @ManyToOne(() => Equipamento, { nullable: true })
+    @JoinColumn({name: "idEquipamento"})
+    equipamento?: Equipamento;
+
+    @Column({type:"varchar", nullable:false})
+    nomeUsuario!: string;
+
+    @Column({type:"varchar", unique:true, nullable:false})
+    emailUsuario!: string;
+
+    @Column({type:"varchar", nullable:false})
+    senhaUsuario!: string;
+
+    @Column({type:"varchar", nullable:false})
+    senhaHash!: string;
+
+    @Column({type:"enum", enum: enumPerfil, nullable:true})
+    perfilUsuario!: enumPerfil;
+
+    @Column({type:"boolean", default:true})
+    statusUsuario!: boolean;
+}
