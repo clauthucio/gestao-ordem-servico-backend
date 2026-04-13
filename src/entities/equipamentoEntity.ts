079193a514fb03f21747780b10e9614fc871ac7a
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 20:27:41 -0400

    Renomeia equipamento.ts para equipamentoEntity.ts

---
+++ src/entities/equipamentoEntity.ts
@@ -0,0 +1,30 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm"
+import {enumTipoEquipamento} from "../types/TipoEquipamento"
+
+@Entity("equipamentos")
+export class Equipamento {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({type:"varchar", nullable:false, unique:true})
+    codigo!: string;
+
+    @Column({type:"varchar", nullable:false})
+    nome!: string;
+
+    @Column({type:"enum", enum: enumTipoEquipamento, nullable:false})
+    tipo!: enumTipoEquipamento;
+
+    @Column({type:"varchar", nullable:false})
+    localizacao!: string;
+
+    @Column({type:"varchar", nullable:true})
+    fabricante!: string;
+
+    @Column({type:"varchar", nullable:true})
+    modelo!: string;
+
+    @Column({type:"boolean", nullable:true, default:true})
+    ativo!: boolean;
+
+}
