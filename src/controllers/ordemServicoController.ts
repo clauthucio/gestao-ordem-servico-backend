3577db76d2cb84e26947b25f375d0a16a22dc0a0
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-12 22:29:27 -0400

    Adiciona Controller Ordem de Servico

---
+++ src/controllers/ordemServicoController.ts
@@ -0,0 +1,45 @@
+import type OrdemServicoService from "../services/ordemServicoService";
+import type { Request, Response } from "express";
+import { CreateOrdemServicoDTO } from "../schemas/ordemServicoSchema";
+import { UpdateOrdemServicoDTO } from './../schemas/ordemServicoSchema';
+
+export default class OrdemServicoController {
+    private ordemServicoService: OrdemServicoService;
+
+    constructor(ordemServicoService: OrdemServicoService){
+        this.ordemServicoService = ordemServicoService;
+    }
+
+    //GET
+    public async findAll (req: Request, res: Response){
+        const OrdemServico = await this.ordemServicoService.findAll();
+        return res.status(200).json(OrdemServico);
+    }
+
+    public async findById (req: Request, res: Response){
+        const OrdemServico = await this.ordemServicoService.findById(req.params.id as string);
+        return res.status(200).json(OrdemServico);
+    }
+
+    //CREATE
+    public async createOrdemServico (req: Request, res: Response){
+        const OrdemServico = await this.ordemServicoService.createOrdemServico(req.body as CreateOrdemServicoDTO);
+        return res.status(201).json(OrdemServico);
+    }
+
+    //UPDATE
+    public async updateOrdemServico (req: Request, res: Response){
+        const OrdemServico = await this.ordemServicoService.updateOrdemServico(
+            req.params.id as string,
+            req.body as UpdateOrdemServicoDTO
+        );
+        return res.status(200).json(OrdemServico);
+    }
+
+    //DELETE
+    public async deleteOrdemServico (req: Request, res: Response){
+        await this.ordemServicoService.deleteOrdemServico(req.params.id as string);
+        return res.status(204).send();
+    }
+        
+}
