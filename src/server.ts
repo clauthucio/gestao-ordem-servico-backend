dc32c732e9d319937ebae85bada9d67217cb4ab3
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-20 15:03:56 -0400

    Adicionado as rotas EUIPAMENTO e AUTENTICAÇÃO no arquivo server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,18 @@ import { dashboardRoute } from './routes/dashboardRoute.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { ordemServicoRoute } from './routes/ordemServicoRoute.js';
 import { usuarioRoute } from './routes/usuarioRoute.js';
+import { equipamentoRoute } from './routes/equipamentoRoute.js';
+import { authRoute } from './routes/authRoute.js';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
+app.use(authRoute);
 app.use(dashboardRoute);
 app.use(usuarioRoute);
 app.use(ordemServicoRoute);
+app.use(equipamentoRoute);
 app.use(errorHandler);
 
 
