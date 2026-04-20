d53c1323b4a009dc458ebb64286511af2386f222
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-20 15:05:14 -0400

    Refatorado a rota de Ordem e Servico - Removido "bind"

--- src/routes/ordemServicoRoute.ts
+++ src/routes/ordemServicoRoute.ts
@@ -1,19 +1,32 @@
-import OrdemServicoController from './../controllers/ordemServicoController';
-import OrdemServicoService from './../services/ordemServicoService';
-import { OrdemServicoCreateSchema, OrdemServicoUpdateSchema } from '../schemas/ordemServicoSchema';
+import { Router, Request, Response } from "express";
+import OrdemServicoController from "./../controllers/ordemServicoController";
+import OrdemServicoService from "./../services/ordemServicoService";
+import { OrdemServicoCreateSchema, OrdemServicoUpdateSchema } from "../schemas/ordemServicoSchema";
 import { validateBody } from "../middlewares/validateBody";
-import { Router } from "express";
 
 const ordemServicoRoute = Router();
-
 const ordemServicoService = new OrdemServicoService();
-const ordemServicoController = new OrdemServicoController(ordemServicoService)
+const ordemServicoController = new OrdemServicoController(ordemServicoService);
 
-ordemServicoRoute.get("/app/os", ordemServicoController.findAll.bind(ordemServicoController));
-ordemServicoRoute.get("/app/os/:id", ordemServicoController.findById.bind(ordemServicoController));
-ordemServicoRoute.post("/app/os", validateBody(OrdemServicoCreateSchema), ordemServicoController.createOrdemServico.bind(ordemServicoController));
-ordemServicoRoute.put("/app/os/:id", validateBody(OrdemServicoUpdateSchema), ordemServicoController.updateOrdemServico.bind(ordemServicoController));
-ordemServicoRoute.delete("/app/os/:id",ordemServicoController.deleteOrdemServico.bind(ordemServicoController));
+ordemServicoRoute.get("/app/os", async (req: Request, res: Response) => {
+  await ordemServicoController.findAll(req, res);
+});
+
+ordemServicoRoute.get("/app/os/:id", async (req: Request, res: Response) => {
+  await ordemServicoController.findById(req, res);
+});
+
+ordemServicoRoute.post("/app/os", validateBody(OrdemServicoCreateSchema), async (req: Request, res: Response) => {
+  await ordemServicoController.create(req, res);
+});
+
+ordemServicoRoute.put("/app/os/:id", validateBody(OrdemServicoUpdateSchema), async (req: Request, res: Response) => {
+  await ordemServicoController.update(req, res);
+});
+
+ordemServicoRoute.delete("/app/os/:id", async (req: Request, res: Response) => {
+  await ordemServicoController.delete(req, res);
+});
 
 export { ordemServicoRoute };
 
