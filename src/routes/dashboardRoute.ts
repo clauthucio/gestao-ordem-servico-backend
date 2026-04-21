7b95d13e5227e5cd65e6240249914922377e48d6
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2026-04-20 22:46:19 -0400

    Refatoração Rota de Usuário - Inserido autenticação por perfil

--- src/routes/dashboardRoute.ts
+++ src/routes/dashboardRoute.ts
@@ -1,13 +1,19 @@
 import { DashboardService } from "../services/dashboardService";
 import { DashboardController } from "../controllers/dashboardController";
 import { Router, Request, Response } from "express";
+import { AuthenticateToken } from "../middlewares/authMiddleware";
 
 const dashboardRoute = Router();
 const dashboardService = new DashboardService();
 const dashboardController = new DashboardController(dashboardService);
 
-dashboardRoute.get("/app/dashboard", async (req: Request, res: Response) => {
+// GET: Retorna dados do dashboard (todos usuários autenticados)
+dashboardRoute.get(
+  "/app/dashboard",
+  AuthenticateToken,
+  async (req: Request, res: Response) => {
     await dashboardController.getDashboard(req, res);
-});
+  }
+);
 
 export { dashboardRoute };
